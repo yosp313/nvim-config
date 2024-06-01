29a78a0cf882b5515ace50d7ef8969eafa313025
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-06-01 19:35:23 +0300

    Added Supermaven and php intelisense

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -52,6 +52,10 @@ return {
         capabilities = capabilities
       })
 
+      lspconfig.intelephense.setup({
+        capabilities = capabilities,
+      })
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
