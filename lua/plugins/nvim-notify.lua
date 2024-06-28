8e2486b129064c6e5882a326215916e62e8af020
Youssef Ayman <yosp31@gmail.com>
2024-06-29 02:36:47 +0300

    added nvim-notify plugin

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,3 +1,11 @@
 return{
     "rcarriga/nvim-notify",
+    config = function()
+        require("notify").setup({
+            background_colour = "#000000",
+            render = "minimal",
+            timeout = 1000,
+            stages = "slide",
+        })
+  end
 }
